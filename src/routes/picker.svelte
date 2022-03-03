baf79581f4cc5e74384eb8f36b89814a2593231f
Valentin Py <valentin.pyb@gmail.com>
2022-03-03 07:46:38 +0100

    clean props handler picker

--- src/routes/picker.svelte
+++ src/routes/picker.svelte
@@ -2,6 +2,13 @@
 </script>
 <script lang="ts">
     import Picker from 'svelte-material-color-picker';
+    let useSpectrumPicker = true;
+    let defaultTint = 500;
+    let colorsPerRow = 6;
+    let mode = "palette";
+    let size = 50;
+    let margin = 1;
+
     let mainColor = '#f44336';
     let fontColor = pickTextColorBasedOnBgColorAdvanced(mainColor, '#fff', '#000');
     function colorHandler(e){
@@ -26,10 +33,43 @@
     }
 
 </script>
+
 <div class="container">
+    <div class="props">
+        <div class="props-container">
+            <label class="label" for="useSpectrum" style:background-color="{mainColor}" style:color="{fontColor}" style:border-color="{mainColor}">useSpectrum</label>
+            <span class="input"><input type="checkbox" id="useSpectrum" bind:checked={useSpectrumPicker} /></span>
+            <span class="value">{useSpectrumPicker}</span>
+        </div>
+
+        <div class="props-container">
+            <label class="label" for="defaultTint" style:background-color="{mainColor}" style:color="{fontColor}" style:border-color="{mainColor}">default tint</label>
+            <span class="input"><input type="range" id="defaultTint" min="100" max="900" step="100" bind:value={defaultTint} /></span>
+            <span class="value">{defaultTint}</span>
+        </div>
+
+        <div class="props-container">
+            <label class="label" for="size" style:background-color="{mainColor}" style:color="{fontColor}" style:border-color="{mainColor}">size</label>
+            <span class="input"><input type="range" id="size" min="10" max="100" step="10" bind:value={size}/></span>
+            <span class="value">{size}</span>
+        </div>
+
+        <div class="props-container">
+            <label class="label" for="margin" style:background-color="{mainColor}" style:color="{fontColor}" style:border-color="{mainColor}">margin</label>
+            <span class="input"><input type="range" id="margin" min="0" max="10" step="1" bind:value={margin} /></span>
+            <span class="value">{margin}</span>
+        </div>
+
+        <div class="props-container">
+            <label class="label" for="colorsPerRow" style:background-color="{mainColor}" style:color="{fontColor}" style:border-color="{mainColor}">colors per row</label>
+            <span class="input"><input type="range" id="colorsPerRow" min="1" max="10" step="1" bind:value={colorsPerRow} /></span>
+            <span class="value">{colorsPerRow}</span>
+        </div>
+    </div>
+
     <div class="chosen"><span class="badge" style:background-color={mainColor} style:color={fontColor}>{mainColor ? mainColor : "none"} </span></div>
     <div class="picker-container" style:border-color="{mainColor}">
-        <Picker on:colorChanged={colorHandler}/>
+        <Picker {useSpectrumPicker} {defaultTint} {size} {margin} {colorsPerRow} on:colorChanged={colorHandler}/>
     </div>
 </div>
 
@@ -44,6 +84,33 @@
         font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";
 
     }
+
+    .props{
+        position:absolute;
+        top:30px;
+        left:0px;
+        border-radius:5px;
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items:center;
+        /* width:150px; */
+        background: white;
+        z-index: 3;
+        border: 1px solid rgba(0,0,0,0.25);
+        padding: 5px;
+    }
+
+    .props-container{
+        /* display:flex;
+        justify-content: center;
+        align-items: center; */
+        display:grid;
+        grid-template-columns: 100px 120px 50px;
+        grid-template-areas: "label input value";
+        margin-bottom: 10px;
+    }
+
     .picker-container{
         position:absolute;
         top:150px;
@@ -62,5 +129,30 @@
     .badge{
         padding: 5px 10px;
         border-radius:50px;
+    }
+
+    .label{
+        grid-area:label;
+        font-size:12px;
+        text-align:center;
+        border:1px solid;
+        border-radius:8px;
+        padding:5px;
+    }
+
+    .value{
+        grid-area:value;
+        font-size:12px;
+
+    }
+
+    .input{
+        grid-area:input;
+        display:inline-block;
+        text-align:center;
+    }
+
+    input[type=range] {
+        width:100px;
     }
 </style>
